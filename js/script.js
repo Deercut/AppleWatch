9632cf8247696ffc4ec28851ff8a1917c4826fa8
Valentin <val.zawar@gmail.com>
2023-01-11 00:43:01 +0100

    [feature/html-search] Start to adjuste the responsive size for the header.

--- js/script.js
+++ js/script.js
@@ -2,15 +2,21 @@
 //Simple condition pour retirer notre croix
 
 const $menuBtn = document.querySelector(".menu-button");
+const $mainNav = document.querySelector(".main-nav");
+const $bag = document.querySelector(".bag");
 
 // use the classList API to remove and add classes
 
 function clickMenuButton(){
     if($menuBtn.classList.contains("-active")){
         $menuBtn.classList.remove("-active")
+        $mainNav.classList.remove("-active")
+        $bag.classList.remove("-hidden");
     }
     else{
         $menuBtn.classList.add("-active");
+        $mainNav.classList.add("-active");
+        $bag.classList.add("-hidden");
     }
 }
 
